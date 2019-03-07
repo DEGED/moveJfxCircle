dedb8e3e548cadf4f20ddada60f1bb5a864fdd18
DEGED <johann0117@live.com>
2019-03-07 18:01:12 -0500

    javafx

--- src/threads/SimpleThreadMove.java
+++ src/threads/SimpleThreadMove.java
@@ -1,26 +1,35 @@
 package threads;
 
 import ui.CircleController;
-
+import javafx.scene.shape.Rectangle;
+import java.util.*;
 public class SimpleThreadMove extends Thread{
 	
 	private CircleController cc;
 	private boolean c;
 	private boolean r;
-	
-	public SimpleThreadMove(CircleController cc, boolean r) {
+	private int id;
+	private ArrayList<Rectangle> rectangles;
+//	Rectangle p
+	public SimpleThreadMove(CircleController cc, boolean r, int idd, Rectangle x) {
 		this.cc = cc;
 		c = true;
 		this.r = r;
+		id = idd;
+		rectangles  = new ArrayList<Rectangle>();
+		rectangles.add(x);
+		
+		
 	}
 	
 	public void run() {
 		while(c) {
-			
+			cc.setShape(rectangles.get(id));
 			r = cc.rigth(r);
 			r = cc.left(r);
 			try {
 				sleep(30);
+				
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
--- src/ui/CircleController.java
+++ src/ui/CircleController.java
@@ -1,10 +1,15 @@
 package ui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.ResourceBundle;
+
+import javafx.animation.ParallelTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import threads.SimpleThreadMove;
@@ -29,19 +34,47 @@ public class CircleController {
     @FXML
     private Button stop;
     
+    @FXML
+    private Button DownButt;
+    
     private SimpleThreadMove t;
     
     private boolean r;
     
+    private Rectangle square;
+    private ArrayList<Rectangle> po;
+//    private ArrayList<SimpleThreadMove> threads;
+    
+    @FXML
+    void DownMethod(ActionEvent event) {
+    	shape.setLayoutY(pane.getHeight());
+    }
+
+    @FXML
+    void generateNew(MouseEvent event) {
+    	int idd = 1;
+    	
+    	double x = event.getX();
+    	double y = event.getY();
+//    	ArrayList<Rectangle> squareList = new  ArrayList<Rectangle>();
+    	shape = new Rectangle(x,y, shape.getWidth(),shape.getHeight());
+    	SimpleThreadMove xio = new SimpleThreadMove(this,r,idd,shape);
+    	pane.getChildren().add(po.get(idd));
+    	xio.start();
+    	
+//    	SimpleThreadMove l = new SimpleThreadMove(this, r, idd);
+//    	l.start();
+//    	threads.add(l);
+    	idd++;
+    }
     @FXML
     void move(ActionEvent event) {
-    	t = new SimpleThreadMove(this,r);
+    	t = new SimpleThreadMove(this,r,0,shape);
     	t.start();
     	
     	stop.setDisable(false);
     	move.setDisable(true);
     }
-    
 	@FXML
     void stop(ActionEvent event) {
     	stop.setDisable(true);
@@ -55,34 +88,42 @@ public class CircleController {
     		shape.setLayoutX(shape.getLayoutX()+5);
     		shape.setLayoutY(shape.getLayoutY()+5);
     		shape.setRotate(shape.getRotate()+4);
+    		shape.
     		System.out.println("Derecha");
     	}
-    	if(shape.getLayoutX()>=pane.getWidth()-shape.getWidth() || shape.getLayoutY()>=pane.getHeight()) {
+// 	 || shape.getLayoutY()>=pane.getMaxHeight()
+    	if(shape.getLayoutX()>=pane.getWidth() || shape.getLayoutY()>=pane.getHeight()) {
 			r = false;
 		}
 		return r;
     }
-    
-    public boolean left(boolean r) {
+    ParallelTransition poo= new ParallelTransition();
+    public boolean left(boolean r){
     	if(!r){
     		shape.setLayoutX(shape.getLayoutX()-5);
     		shape.setLayoutY(shape.getLayoutY()-5);
     		shape.setRotate(shape.getRotate()-4);
     		System.out.println("Izquierda");
     	}
-    	if(shape.getLayoutX()<=0 || shape.getLayoutY()<=0) {
+//    	 || pane.getLayoutY() <= 0
+    	if(shape.getLayoutX() <= 0 || shape.getLayoutY() <= 0) {
 			r = true;
 		}	
 		return r;
     }
     	
-    
-    
+    public void setShape(Rectangle l) {
+    	shape = l;
+    }
+    public boolean getR() {
+    	return r;
+    }
     @FXML
     void initialize() {
         assert pane != null : "fx:id=\"pane\" was not injected: check your FXML file 'Circle.fxml'.";
         assert shape != null : "fx:id=\"shape\" was not injected: check your FXML file 'Circle.fxml'.";
-
+//        id = 0;
+        po = new ArrayList<Rectangle>();
     }
 }
 
